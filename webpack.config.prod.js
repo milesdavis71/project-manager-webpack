1cae033007683f395a73841cedd8d107a453641a
milesdavis71 <hevesiistvan71@hotmail.com>
2022-02-21 14:41:52 +0100

    webpack.config.prod.js inicializálása.

---
+++ webpack.config.prod.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const cleanPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [new cleanPlugin.CleanWebpackPlugin()],
+};
