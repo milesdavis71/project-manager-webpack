daf27d4dc3a23629430f3c194d9208e692e63aeb
milesdavis71 <hevesiistvan71@hotmail.com>
2022-02-24 08:35:13 +0100

    ProjectItem osztályban a get persons implementálása kész.

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -3,6 +3,15 @@ import Component from './base-component';
 
 export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person';
+    }
+    this.project.people > 1;
+    return `${this.project.people} persons`;
+  }
+
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -12,8 +21,7 @@ export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   config() {}
   renderContent() {
     this.element.querySelector('h2')!.innerHTML = this.project.title;
-    this.element.querySelector('h3')!.innerHTML =
-      this.project.people.toString();
+    this.element.querySelector('h3')!.innerHTML = `${this.persons} assigned`;
     this.element.querySelector('p')!.innerHTML = this.project.description;
   }
 }
